bfc661908eb3f813716ea3fd523ea8fdf89c0768
HamzaOubada2 <oubham220@gmail.com>
2026-02-18 17:20:17 +0000

    Footer Update 2

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,34 +3,43 @@ import styles from "./Contact.module.css";
 import Link from "next/link";
 
 export default function Contact() {
-    return (
-        <section id="contact" className="container section">
-            <div className={styles.wrapper}>
-                <h2 className="section-title">
-                    <span className="text-gradient">Get in Touch</span>
-                </h2>
+  return (
+    <section id="contact" className="container section">
+      <div className={styles.wrapper}>
+        <h2 className="section-title">
+          <span className="text-gradient">Get in Touch</span>
+        </h2>
 
-                <p className={styles.text}>
-                    Ideally I am looking for opportunities where I can solve complex backend challenges.
-                    Whether you have a question or just want to say hi, I'll try my best to get back to you!
-                </p>
+        <p className={styles.text}>
+          Ideally I am looking for opportunities where I can solve complex
+          backend challenges. Whether you have a question or just want to say
+          hi, I'll try my best to get back to you!
+        </p>
 
-                <Link href="mailto:hello@example.com" className={styles.chatBtn}>
-                    <Mail size={20} />
-                    Say Hello
-                </Link>
+        <Link href="mailto:hello@example.com" className={styles.chatBtn}>
+          <Mail size={20} />
+          Say Hello
+        </Link>
 
-                <div className={styles.socials}>
-                    <Link href="https://www.linkedin.com/in/hamza-oubada-4aa94b340/" target="_blank" className={styles.socialLink}>
-                        <Linkedin size={24} />
-                        <span>LinkedIn</span>
-                    </Link>
-                    <Link href="https://github.com/HamzaOubada2" target="_blank" className={styles.socialLink}>
-                        <Github size={24} />
-                        <span>GitHub</span>
-                    </Link>
-                </div>
-            </div>
-        </section>
-    );
+        <div className={styles.socials}>
+          <Link
+            href="https://github.com/HamzaOubada2"
+            target="_blank"
+            className={styles.socialLink}
+          >
+            <Linkedin size={24} />
+            <span>LinkedIn</span>
+          </Link>
+          <Link
+            href="https://github.com/HamzaOubada2"
+            target="_blank"
+            className={styles.socialLink}
+          >
+            <Github size={24} />
+            <span>GitHub</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
